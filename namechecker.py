25392adcab1109f3bb12d2ae16f62398dc74763e
Alex Z <alexzheng@hotmail.nl>
2023-06-14 01:42:15 +0200

    some fixes

--- namechecker.py
+++ namechecker.py
@@ -1,6 +1,7 @@
 import requests
 import threading
 import time
+import os
 
 # Read the file and extract the variables
 with open("env.txt", "r") as file:
@@ -13,13 +14,18 @@ with open("env.txt", "r") as file:
 with open("tokens.txt", "r") as file:
     tokens, passwords = zip(*(line.strip().split(":") for line in file))
 
+# Read the usernames from the file
+with open(tocheck, "r") as file:
+    usernames = [word for line in file for word in line.strip().split()]
+
+
 # Split username list for each thread to process
-def process_usernames(start, end, token, password):
+def process_usernames(start, end, token, password, run_event):
     for i in range(start, end):
         if i >= len(usernames):
             break
         username = usernames[i]
-        url = "https://discord.com/api/v10/users/@me"
+        url = "https://discord.com/api/v9/users/@me"
         headers = {
             "accept": "*/*",
             "authorization": token,
@@ -29,19 +35,21 @@ def process_usernames(start, end, token, password):
             "username": username,
             "password": password
         }
+        # Check if the thread should stop
+        if not run_event.is_set():
+            break
         response = requests.patch(url, headers=headers, json=payload)
         data = response.json()
-
-        if "code" in data and data.get('code') == 50035:
+        if data.get('code', 0) == 50035:
             handle_taken(data, username)
-        elif "code" in data and (data.get('code') == 10020 or 'captcha_key' in data):
+        elif (data.get('code', 0) == 10020 or 'captcha_key' in data):
             handle_available(data, username)
         elif "retry_after" in data:
-            if handle_rate_limited(data, username, url, headers, payload):
-                break  # only break if unknown error
+            time.sleep(2.5)  # Sleep 2.5 seconds to avoid rate limit
+            handle_rate_limited(data, username, url, headers, payload)
         else:
-            if handle_unknown(data):
-                break  # only break if unknown error
+            print(f"Unknown error or 401, token: {token}, pass:{password}")
+            handle_unknown(data)
 
         time.sleep(2.5)  # Sleep 2.5 seconds to avoid rate limit
 
@@ -57,16 +65,14 @@ def handle_rate_limited(data, username, url, headers, payload):
     response = requests.patch(url, headers=headers, json=payload)  # try again
     data = response.json()
 
-    if "code" in data and data.get('code') == 50035:
+    if data.get('code', 0) == 50035:
         handle_taken(data, username)
-        return False
-    elif "code" in data and (data.get('code') == 10020 or 'captcha_key' in data):
+    elif (data.get('code', 0) == 10020 or 'captcha_key' in data):
         handle_available(data, username)
-        return False
     elif "retry_after" in data:
-        return handle_rate_limited(data, username, url, headers, payload)
+        handle_rate_limited(data, username, url, headers, payload)
     else:
-        return handle_unknown(data)
+        handle_unknown(data)
 
 def handle_available(data, username):
     print(data)
@@ -76,12 +82,11 @@ def handle_available(data, username):
 
 def handle_unknown(data):
     print(data)
-    print("Unknown error code, stopped the script")
-    return True
+    os._exit(1)
 
-# Read the usernames from the file
-with open(tocheck, "r") as file:
-    usernames = [word for line in file for word in line.strip().split()]
+# Create an event object to signal threads
+run_event = threading.Event()
+run_event.set()
 
 # Number of threads
 num_threads = min(len(tokens), len(usernames))
@@ -92,10 +97,17 @@ chunk_size = len(usernames) // num_threads
 for i, (token, password) in enumerate(zip(tokens, passwords)):
     start = i * chunk_size
     end = start + chunk_size + (1 if i < len(usernames) % num_threads else 0)
-    thread = threading.Thread(target=process_usernames, args=(start, end, token, password))
+    thread = threading.Thread(target=process_usernames, args=(start, end, token, password, run_event))
     threads.append(thread)
     thread.start()
 
-# Wait for all threads to finish
-for thread in threads:
-    thread.join()
+# Wait for keyboard interrupt
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("Attempting to close threads")
+    run_event.clear() # Signal threads to stop
+    for thread in threads:
+        thread.join() # Wait for threads to finish
+    print("Threads successfully closed")
