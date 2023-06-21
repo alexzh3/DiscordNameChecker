41d41b8147cdb7228a1e3a21a4e3b88e52250737
Alex Z <alexzheng@hotmail.nl>
2023-06-21 14:57:03 +0200

    added log instead of prints

--- namechecker.py
+++ namechecker.py
@@ -3,6 +3,9 @@ import threading
 import time
 import os
 from tqdm import tqdm
+import logging
+import datetime
+
 
 # Read the file and extract the variables
 with open("env.txt", "r") as file:
@@ -14,7 +17,32 @@ with open("env.txt", "r") as file:
     notifications = env_dict.get("notifications") == "True"
     bot_token = env_dict.get("bot_token")
     chat_id = env_dict.get("chat_id")
+    log_type = env_dict.get("log_type")
 
+# Map log type string to corresponding log level constant
+log_level_mapping = {
+    "DEBUG": logging.DEBUG,
+    "INFO": logging.INFO,
+    "WARNING": logging.WARNING,
+    "ERROR": logging.ERROR,
+    "CRITICAL": logging.CRITICAL
+}
+
+# Configure logging
+current_datetime = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+log_file = f"log_{current_datetime}.txt"
+logging.basicConfig(
+    level=log_level_mapping[log_type],
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.StreamHandler(),  # Display logs in the terminal
+        logging.FileHandler(log_file)  # Write logs to the file
+    ]
+)
+
+# Set the log level for the 'requests' and 'urllib3' library to a higher level than application's log level
+logging.getLogger('requests').setLevel(logging.WARNING)
+logging.getLogger('urllib3').setLevel(logging.WARNING)
 
 # Read the tokens and passwords from the file
 with open("tokens.txt", "r") as file:
@@ -51,7 +79,7 @@ def process_usernames(token, password, run_event, progress_bar):
             time.sleep(2.5)  # Sleep 2.5 seconds to avoid rate limit
             handle_rate_limited(data, username, url, headers, payload)
         else:
-            print(f"Unknown error or 401, token: {token}, pass:{password}")
+            logging.error(f"Unknown error or 401, token: {token}, pass:{password}, {threading.current_thread().name}")
             handle_unknown(data)
 
         time.sleep(2.5)  # Sleep 2.5 seconds to avoid rate limit
@@ -62,13 +90,12 @@ def process_usernames(token, password, run_event, progress_bar):
 
 
 def handle_taken(data, username):
-    print(data)
-    print(f'{username} is already taken')
-
+    logging.debug(f" {threading.current_thread().name} - {data}")
+    logging.info(f" {threading.current_thread().name} - {username} is already taken")
 
 def handle_rate_limited(data, username, url, headers, payload):
-    print(data)
-    print(f"Rate limited, waiting {data.get('retry_after') + 0.1} seconds")
+    logging.debug(f"{threading.current_thread().name} - {data}")
+    logging.info(f"{threading.current_thread().name} - Rate limited, waiting {data.get('retry_after') + 0.1} seconds")
     time.sleep(data.get('retry_after') + 0.1)  # Wait for rate limit
 
     response = requests.patch(url, headers=headers, json=payload)  # try again
@@ -83,10 +110,9 @@ def handle_rate_limited(data, username, url, headers, payload):
     else:
         handle_unknown(data)
 
-
 def handle_available(data, username):
-    print(data)
-    print(f'{username} is available and is added to the text file')
+    logging.debug(f"{threading.current_thread().name} - {data}")
+    logging.info(f"{threading.current_thread().name} - {username} is available and is added to the text file")
     with open(available, 'a') as file:
         file.write(str(username) + '\n')
     if notifications == True:
@@ -97,7 +123,7 @@ def handle_available(data, username):
 
 
 def handle_unknown(data):
-    print(data)
+    logging.error(f"{threading.current_thread().name} - {data}")
     os._exit(1)
 
 # Replace with your own Telegram bot token and chat ID
@@ -109,7 +135,7 @@ def send_telegram_message(token, chat_id, message):
     }
     response = requests.post(url, params=params)
     if response.status_code != 200:
-        print("Failed to send Telegram message.")
+        logging.error("Failed to send Telegram message.")
 
 
 # Create an event object to signal threads
@@ -123,7 +149,9 @@ num_threads = min(len(tokens), len(usernames))
 threads = []
 with tqdm(total=len(usernames), desc="Checking usernames") as progress_bar:
     for i, (token, password) in enumerate(zip(tokens, passwords)):
-        thread = threading.Thread(target=process_usernames, args=(token, password, run_event, progress_bar))
+        thread_num = i + 1  # Thread number starts from 1
+        thread_name = f"Thread-{thread_num}"
+        thread = threading.Thread(target=process_usernames, args=(token, password, run_event, progress_bar), name=thread_name)
         threads.append(thread)
         thread.start()
 
@@ -132,8 +160,8 @@ with tqdm(total=len(usernames), desc="Checking usernames") as progress_bar:
         while True:
             time.sleep(1)
     except KeyboardInterrupt:
-        print("Attempting to close threads")
+        logging.info("Attempting to close threads")
         run_event.clear()  # Signal threads to stop
         for thread in threads:
             thread.join()  # Wait for threads to finish
-        print("Threads successfully closed")
+        logging.info("Threads successfully closed")
