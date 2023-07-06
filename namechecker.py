e60b1eacd09b705a4132fba6b695734911f2f598
Alex Z <alexzheng@hotmail.nl>
2023-07-06 04:06:38 +0200

    Working version

--- namechecker.py
+++ namechecker.py
@@ -6,94 +6,111 @@ from tqdm import tqdm
 import logging
 import datetime
 from itertools import cycle
-
-# Read the file and extract the variables
-with open("env.txt", "r") as file:
-    env_data = [line.strip().split("=") for line in file]
-    env_dict = dict(env_data)
-    tocheck = env_dict.get("tocheck")
-    available = env_dict.get("available")
-    loop = env_dict.get("loop") == "True"
-    notifications = env_dict.get("notifications") == "True"
-    bot_token = env_dict.get("bot_token")
-    chat_id = env_dict.get("chat_id")
-    log_type = env_dict.get("log_type")
-    proxy_enabled = env_dict.get("proxy_enabled") == "True"
-    proxy_token = env_dict.get("proxy_token")
+import random
 
 
-# Configure logging
-current_datetime = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-log_file = f"log_{current_datetime}.txt"
+def read_env_file(file_path):
+    with open(file_path, "r") as file:
+        env_data = [line.strip().split("=") for line in file]
+        env_dict = dict(env_data)
+        return env_dict
 
-handlers = [logging.StreamHandler()]  # Always display logs in the terminal
 
-# Map log type string to corresponding log level constant
-log_level_mapping = {
-    "DEBUG": logging.DEBUG,
-    "INFO": logging.INFO,
-    "WARNING": logging.WARNING,
-    "ERROR": logging.ERROR,
-    "CRITICAL": logging.CRITICAL,
-}
+def configure_logging(log_type):
+    current_datetime = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    log_file = f"log_{current_datetime}.txt"
+    handlers = [logging.StreamHandler()]  # Always display logs in the terminal
 
-if log_level_mapping[log_type] == logging.DEBUG:
-    handlers.append(logging.FileHandler(log_file))  # Add file handler for DEBUG level
+    log_level_mapping = {
+        "DEBUG": logging.DEBUG,
+        "INFO": logging.INFO,
+        "WARNING": logging.WARNING,
+        "ERROR": logging.ERROR,
+        "CRITICAL": logging.CRITICAL,
+    }
 
-logging.basicConfig(
-    level=log_level_mapping[log_type],
-    format="%(asctime)s - %(levelname)s - %(message)s",
-    handlers=handlers,
-)
+    if log_level_mapping[log_type] == logging.DEBUG:
+        handlers.append(
+            logging.FileHandler(log_file)
+        )  # Add file handler for DEBUG level
 
-# Set the log level for the 'requests' and 'urllib3' library to a higher level than application's log level
-logging.getLogger("requests").setLevel(logging.WARNING)
-logging.getLogger("urllib3").setLevel(logging.WARNING)
+    logging.basicConfig(
+        level=log_level_mapping[log_type],
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        handlers=handlers,
+    )
+
+    logging.getLogger("requests").setLevel(logging.WARNING)
+    logging.getLogger("urllib3").setLevel(logging.WARNING)
+
+
+def create_proxy_pool(proxy_enabled, proxy_token):
+    proxies_formatted = []
+    if proxy_enabled:
+        # Retrieve list of proxies from API
+        response = requests.get(
+            f"https://proxy.webshare.io/api/v2/proxy/list/download/{proxy_token}/-/any/username/direct/-/"
+        )
+        proxy_list = response.text.splitlines()
+
+        # Modify the format of proxies
+        for proxy in proxy_list:
+            ip, port, username, password = proxy.split(":")
+            proxy_formatted = f"{username}:{password}@{ip}:{port}"
+            proxies_formatted.append(proxy_formatted)
+
+    # Set iterator
+    proxy_pool = cycle(proxies_formatted)
+    return proxy_pool
 
 
 # Setup usernames
 def read_usernames():
     with open(tocheck, "r") as file:
         for line in file:
-            words = line.strip().split()
-            usernames.extend(words)
+            username = line.strip().split()
+            usernames.extend(username)
 
 
-# Read tokens:password
+# Read tokens
 def read_tokens():
     with open("tokens.txt", "r") as file:
         for line in file:
             line = line.strip()
-            if ":" in line:
-                token, password = line.split(":")
-            else:
-                token = line
-                password = ""  # Empty string
+            token = line
             tokens.append(token)
-            passwords.append(password)
-    return tokens, passwords
+    return tokens
 
 
 # Process an username request
-def process_usernames(token, password, run_event, progress_bar):
+def process_usernames(token, run_event, progress_bar, url):
+    global hasLock
     try:
         while run_event.is_set() and usernames:
-            if len(usernames) == 1:
+            if url == url_attempt:
+                time.sleep(20)  # Sleep 20 seconds
+            if len(usernames) == 0:
                 if loop:
-                    # Re-populate the usernames list from the tocheck file
-                    read_usernames()
-                    reset_progress_bar(progress_bar, len(usernames))
+                    if not hasLock:
+                        hasLock =  True
+                        with lock:
+                            read_usernames()    # Re-populate the usernames list from the tocheck file
+                            reset_progress_bar(progress_bar, len(usernames))
+                        hasLock = False
                 else:
-                    time.sleep(6)  # Wait for threads to finish
+                    time.sleep(30)
                     message = f"Done with checking: {tocheck}"
                     send_telegram_message(bot_token, chat_id, message)
                     os._exit(1)
-
-            username = usernames.pop(0)
-            if proxy_enabled is True:
+            try:
+                username = usernames.pop(0)
+            except Exception as e:
+                logging.error(f"Exception occurred while popping username: {str(e)}")
+                continue
+            if proxy_enabled:
                 proxy = next(proxy_pool)
                 proxies = {"http": f"http://{proxy}", "https": f"http://{proxy}"}
-            url = "https://discord.com/api/v9/users/@me"
+            url = url
             headers = {
                 "accept": "*/*",
                 "accept-encoding": "gzip, deflate, br",
@@ -112,18 +129,11 @@ def process_usernames(token, password, run_event, progress_bar):
                 "x-debug-options": "bugReporterEnabled",
                 "x-super-properties": "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwic3lzdGVtX2xvY2FsZSI6Im5sLU5MIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzExNC4wLjAuMCBTYWZhcmkvNTM3LjM2IiwiYnJvd3Nlcl92ZXJzaW9uIjoiMTE0LjAuMC4wIiwib3NfdmVyc2lvbiI6IjEwIiwicmVmZXJyZXIiOiIiLCJyZWZlcnJpbmdfZG9tYWluIjoiIiwicmVmZXJyZXJfY3VycmVudCI6IiIsInJlZmVycmluZ19kb21haW5fY3VycmVudCI6IiIsInJlbGVhc2VfY2hhbm5lbCI6InN0YWJsZSIsImNsaWVudF9idWlsZF9udW1iZXIiOjIwODMxOSwiY2xpZW50X2V2ZW50X3NvdXJjZSI6bnVsbH0",
             }
-            payload = {"username": username, "password": password}
+            payload = {"username": username}
             try:
-                if not password:
-                    url = "https://discord.com/api/v9/users/@me/pomelo"
-                    payload.pop("password")
-                    response = requests.post(
-                        url, headers=headers, json=payload, proxies=proxies, timeout=10
-                    )
-                elif password:
-                    response = requests.patch(
-                        url, headers=headers, json=payload, proxies=proxies, timeout=10
-                    )
+                response = requests.post(
+                    url, headers=headers, json=payload, proxies=proxies, timeout=10
+                )
                 data = response.json()
             except requests.exceptions.RequestException as e:
                 logging.error(
@@ -133,44 +143,55 @@ def process_usernames(token, password, run_event, progress_bar):
                 usernames.append(
                     username
                 )  # Append current username back to list that was not checked because of exception
+                progress_bar.update(-1)  # Update progress bar
+                time.sleep(random.randint(5, 10))
                 continue
 
-            if data.get("code") == 50035 or data.get("username") == username:
+            if (
+                data.get("code") == 50035
+                or data.get("username") == username
+                or data.get("taken") == True
+            ):
                 handle_taken(data, username)
-            elif data.get("code") == 10020 or "captcha_key" in data:
+            elif (
+                data.get("code") == 10020
+                or "captcha_key" in data
+                or data.get("taken") == False
+            ):
                 handle_available(data, username)
             elif data.get("code") == 40002:
                 handle_verify(data, token)
                 usernames.append(
                     username
                 )  # Append current username back to list that was not checked because of error
+                progress_bar.update(-1)  # Update progress bar
                 return  # Stop the current thread
             elif "retry_after" in data:
                 logging.debug(f"{threading.current_thread().name} - {data}")
                 logging.info(
                     f"{threading.current_thread().name} - Rate limited, waiting {data.get('retry_after') + 1} seconds"
                 )
-                usernames.append(
-                    username
-                )  # Append current username back to list that was not checked because of rate limit
                 time.sleep(data.get("retry_after") + 1)  # Wait for rate limit
-                process_usernames(token, password, run_event, progress_bar)  # Retry
+                process_usernames(
+                    token, run_event, progress_bar, url_attempt
+                )  # Retry with attempt url
             elif data.get("code") == 40001:
                 logging.error(f"{threading.current_thread().name} - {data}")
                 logging.error(
-                    f"Token not verified with phone or mail yet, token: {token}, password:{password}, {threading.current_thread().name}"
+                    f"Token not verified with phone or mail yet, token: {token}, {threading.current_thread().name}"
                 )
                 usernames.append(
                     username
                 )  # Append current username back to list that was not checked because of error
+                progress_bar.update(-1)  # Update progress bar
                 return  # Stop current thread
             else:
                 handle_unknown(data)
-            time.sleep(2.5)  # Sleep 2.5 seconds to avoid rate limit
-            progress_bar.update(1)  # Increment progress bar
+            progress_bar.update(1)  # Update progress bar
+
     except Exception as e:
         logging.exception(
-            f"Exception occurred in {threading.current_thread().name}: {str(e)} for token: {token}, password:{password}"
+            f"Exception occurred in {threading.current_thread().name}: {str(e)} for token: {token}"
         )
         os._exit(1)
 
@@ -203,9 +224,7 @@ def handle_verify(data, token):
 
 def handle_unknown(data):
     logging.error(f"{threading.current_thread().name} - {data}")
-    logging.error(
-        f"Unknown error, token: {token}, password:{password}, {threading.current_thread().name}"
-    )
+    logging.error(f"Unknown error, token: {token}, {threading.current_thread().name}")
     os._exit(1)
 
 
@@ -217,11 +236,23 @@ def send_telegram_message(token, chat_id, message):
     if response.status_code != 200:
         logging.error("Failed to send Telegram message.")
 
-
 def reset_progress_bar(progress_bar, total):
     progress_bar.total = total
     progress_bar.reset()
 
+
+# Set config variables
+env_dict = read_env_file("env.txt")
+tocheck = env_dict.get("tocheck")
+available = env_dict.get("available")
+loop = env_dict.get("loop") == "True"
+notifications = env_dict.get("notifications") == "True"
+bot_token = env_dict.get("bot_token")
+chat_id = env_dict.get("chat_id")
+log_type = env_dict.get("log_type")
+proxy_enabled = env_dict.get("proxy_enabled") == "True"
+proxy_token = env_dict.get("proxy_token")
+configure_logging(log_type)
 
 # Read usernames
 usernames = []
@@ -229,24 +260,10 @@ read_usernames()
 
 # Read the tokens and passwords from the file
 tokens = []
-passwords = []
-tokens, passwords = read_tokens()
+tokens = read_tokens()
 
-# Config Proxies
-proxies_formatted = []
-if proxy_enabled is True:
-    # Retrieve list of proxies from API
-    response = requests.get(
-        f"https://proxy.webshare.io/api/v2/proxy/list/download/{proxy_token}/-/any/username/direct/-/"
-    )
-    proxy_list = response.text.splitlines()
-    # Modify the format of proxies
-    for proxy in proxy_list:
-        ip, port, username, password = proxy.split(":")
-        proxy_formatted = f"{username}:{password}@{ip}:{port}"
-        proxies_formatted.append(proxy_formatted)
-# Set iterator
-proxy_pool = cycle(proxies_formatted)
+# Config Proxies, create proxy pool
+proxy_pool = create_proxy_pool(proxy_enabled, proxy_token)
 
 # Create an event object to signal threads
 run_event = threading.Event()
@@ -255,15 +272,26 @@ run_event.set()
 # Number of threads
 num_threads = min(len(tokens), len(usernames))
 
+# Create lock and global bool
+lock = threading.Lock()
+hasLock = False
+
+# URL
+url_attempt = "https://discord.com/api/v9/users/@me/pomelo-attempt"
+
+# Username list length
+usernames_length = len(usernames)
+
 # Create and start the threads
 threads = []
-with tqdm(total=len(usernames), desc="Checking usernames") as progress_bar:
-    for i, (token, password) in enumerate(zip(tokens, passwords)):
-        thread_num = i + 1  # Thread number starts from 1
+with tqdm(total=usernames_length, desc="Checking usernames") as progress_bar:
+    for i, token in enumerate(tokens):
+        time.sleep(0.24)  # Don't start all at the same time
+        thread_num = i  # Thread number starts from 1
         thread_name = f"Thread-{thread_num}"
         thread = threading.Thread(
             target=process_usernames,
-            args=(token, password, run_event, progress_bar),
+            args=(token, run_event, progress_bar, url_attempt),
             name=thread_name,
         )
         threads.append(thread)
